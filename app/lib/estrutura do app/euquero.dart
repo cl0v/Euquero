ea0b087b07a03c51dbf00f5d98c3b230d15a5e11
rntcursos <rntcursos@gmail.com>
2021-05-03 20:46:25 -0300

    euquero.dart com home

--- app/lib/estrutura do app/euquero.dart
+++ app/lib/estrutura do app/euquero.dart
@@ -1,10 +1,11 @@
+import 'package:app/estrutura%20do%20app/pages/home.dart';
 import 'package:flutter/material.dart';
 
 class Euquero extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Home(),
     );
   }
 }
