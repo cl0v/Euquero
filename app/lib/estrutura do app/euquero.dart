1dc92ede5aa08207fd39573ffe5da451c6a314de
rntcursos <rntcursos@gmail.com>
2021-05-04 12:02:38 -0300

    Removi o widget store2

--- app/lib/estrutura do app/euquero.dart
+++ app/lib/estrutura do app/euquero.dart
@@ -1,5 +1,4 @@
 import 'package:app/estrutura%20do%20app/pages/home.dart';
-import 'package:app/estrutura%20do%20app/widgets/stores2.dart';
 import 'package:flutter/material.dart';
 
 class Euquero extends StatelessWidget {
--- app/lib/estrutura do app/pages/home.dart
+++ app/lib/estrutura do app/pages/home.dart
@@ -1,4 +1,3 @@
-import 'package:app/estrutura%20do%20app/widgets/stores2.dart';
 import 'package:flutter/material.dart';
 import 'package:app/estrutura do app/widgets/NavBar.dart';
 import 'package:app/estrutura do app/widgets/stores.dart';
@@ -12,14 +11,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stores2(
-        category: "Pastelaria",
-        discount: 22,
-        id: "Pastel",
-        name: "Pastelaria teste",
-        imagePath: "https://i.ibb.co/FVxxkDJ/Receita-de-Massa-de-Pastel.jpg",
-        price: 12,
-      ),
+      body: Column(),
       bottomNavigationBar: NavBar(),
     );
   }
