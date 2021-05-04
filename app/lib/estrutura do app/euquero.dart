14751de399f3163af470b362b97ffd508f2b4efe
rntcursos <rntcursos@gmail.com>
2021-05-04 11:33:09 -0300

    Teste de widget das lojas

--- app/lib/estrutura do app/euquero.dart
+++ app/lib/estrutura do app/euquero.dart
@@ -1,4 +1,5 @@
 import 'package:app/estrutura%20do%20app/pages/home.dart';
+import 'package:app/estrutura%20do%20app/widgets/stores2.dart';
 import 'package:flutter/material.dart';
 
 class Euquero extends StatelessWidget {
--- app/lib/estrutura do app/pages/home.dart
+++ app/lib/estrutura do app/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:app/estrutura%20do%20app/widgets/stores2.dart';
 import 'package:flutter/material.dart';
 import 'package:app/estrutura do app/widgets/NavBar.dart';
 import 'package:app/estrutura do app/widgets/stores.dart';
@@ -11,12 +12,13 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stores(
-        nome: "Pastelaria Teste",
-        frase: "O melhor pastel da cidade",
-        preco: "15",
-        status: "Aberto",
-        tempo: "45",
+      body: Stores2(
+        category: "Pastelaria",
+        discount: 22,
+        id: "Pastel",
+        name: "Pastelaria teste",
+        imagePath: "https://i.ibb.co/FVxxkDJ/Receita-de-Massa-de-Pastel.jpg",
+        price: 12,
       ),
       bottomNavigationBar: NavBar(),
     );
