1a561aee94d29941f056aa2947bba6a84d361555
rntcursos <rntcursos@gmail.com>
2021-05-08 19:17:27 -0300

    Organizando paginas

--- app/lib/estrutura do app/euquero.dart
+++ app/lib/estrutura do app/euquero.dart
@@ -1,4 +1,4 @@
-import 'package:app/estrutura%20do%20app/pages/home.dart';
+import 'package:app/estrutura%20do%20app/pages/StorePage.dart';
 import 'package:flutter/material.dart';
 
 class Euquero extends StatelessWidget {
@@ -8,7 +8,7 @@ class Euquero extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "Eu Quero Delivery App",
       theme: ThemeData(primaryColor: Colors.amber, accentColor: Colors.amber),
-      home: Home(),
+      home: StorePage(),
     );
   }
 }
